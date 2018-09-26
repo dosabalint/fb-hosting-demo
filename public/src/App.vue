e90358bd508d7a52bea1f573dd4d1df4261c4d2c
dosabalint <dosa.balint@gmail.com>
2018-09-27 00:15:37 +0200

    basic app and db rules

---
+++ public/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app" class="container mt-4 pt-4">
+    <div class="text-center">
+      <img src="./assets/logo.png" class="mb-4">
+    </div>
+    <ContactForm/>
+  </div>
+</template>
+
+<script>
+import ContactForm from "./components/ContactForm.vue";
+export default {
+  name: "app",
+  components: {
+    ContactForm
+  }
+};
+</script>
+
+<style lang="scss">
+@import "~bootstrap/scss/bootstrap";
+
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+</style>
---
+++ public/src/components/ContactForm.vue
@@ -0,0 +1,94 @@
+<template>
+    <form action="">
+        <div class="text-center">
+            <h1>Írj nekünk bátran!</h1>
+        </div>
+    
+        <div class="form-group">
+            <label for="nameInput">Név</label>
+            <input class="form-control" id="nameInput" placeholder="Name" required v-model="name">
+        </div>
+    
+        <div class="form-group">
+            <label for="emailInput">Email</label>
+            <input type="email" class="form-control" id="emailInput" placeholder="name@example.com" required v-model="email">
+        </div>
+    
+        <div class="form-group">
+            <label for="subjectInput">Tárgy</label>
+            <input class="form-control" id="subjectInput" required v-model="subject">
+        </div>
+    
+        <div class="form-group">
+            <label for="messageTextarea">Üzenet</label>
+            <textarea class="form-control" id="messageTextarea" rows="3" v-model="message" required></textarea>
+        </div>
+    
+        <button type="submit" class="btn mb-4" :class="{'btn-primary': isValid, 'btn-secondary': !isValid}" :disabled="!isValid" @click="submit">Küldés</button>
+    
+        <div class="alert alert-success" role="alert" v-if="success">
+            Sikeresen beküldted az üzenetet!
+        </div>
+
+        <div class="alert alert-danger" role="alert" v-if="success === false">
+            A beküldés valamiért sikertelen volt!
+        </div>
+    </form>
+</template>
+
+<script>
+    import axios from 'axios';
+    
+    const fbUrl = "https://my-fb-hosting-demo.firebaseio.com/contactMessages.json";
+    
+    export default {
+        data() {
+            return {
+                name: "dsa",
+                email: "dsa@dsa.hu",
+                subject: "asd",
+                message: "asdfsadfsd",
+                success: null
+            };
+        },
+        computed: {
+            isValid() {
+                return !!this.name &&
+                    /@.*?\./.test(this.email) &&
+                    !!this.subject &&
+                    !!this.message;
+            },
+            formData() {
+                return {
+                    name: this.name,
+                    email: this.email,
+                    subject: this.subject,
+                    message: this.message
+                }
+            }
+        },
+        methods: {
+            submit(event) {
+                if (!this.isValid) return;
+    
+                event.preventDefault();
+                
+                this.success = null;
+                axios.post(fbUrl, this.formData)
+                    .then(response => {
+                        this.success = true;
+                        this.clear();
+                    })
+                    .catch(error => {
+                        this.success = false;
+                    });
+            },
+            clear() {
+                this.name = '';
+                this.email = '';
+                this.subject = '';
+                this.message = '';
+            }
+        }
+    };
+</script>
